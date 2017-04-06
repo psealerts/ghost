0a91b18750445e955cd70dabe3f064793192c213
psealerts <psealerts.me@gmail.com>
2017-04-06 16:55:23 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 var app = require('express')();
 
 app.use(express.static('public'));
-app.listen(80, 139.59.251.145, function(){
+
+app.get('/', function(req, res){
+  res.send("hi");
+});
+
+app.listen(80, '139.59.251.145', function(){
+ console.log("running");
 });
